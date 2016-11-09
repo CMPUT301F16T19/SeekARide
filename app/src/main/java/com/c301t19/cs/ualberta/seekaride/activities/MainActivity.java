a228f44d663b6a18f224385907c483e7f69f2ef2
tommy3ua <firestar_firestar@live.ca>
2016-11-09 13:04:30 -0700

    added testing activity where you can test anything you want. will be removed before final build

--- app/src/main/java/com/c301t19/cs/ualberta/seekaride/activities/MainActivity.java
+++ app/src/main/java/com/c301t19/cs/ualberta/seekaride/activities/MainActivity.java
@@ -1,16 +1,32 @@
 package com.c301t19.cs.ualberta.seekaride.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.c301t19.cs.ualberta.seekaride.R;
 
 
 public class MainActivity extends Activity {
 
+    Button testScreenButton; // remove before final build
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // remove before final build
+        testScreenButton = (Button) findViewById(R.id.testingButton);
+        testScreenButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                startActivity(new Intent(getParent(), TestingActivity.class));
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/c301t19/cs/ualberta/seekaride/activities/TestingActivity.java
@@ -0,0 +1,15 @@
+package com.c301t19.cs.ualberta.seekaride.activities;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.c301t19.cs.ualberta.seekaride.R;
+
+public class TestingActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing);
+    }
+}
