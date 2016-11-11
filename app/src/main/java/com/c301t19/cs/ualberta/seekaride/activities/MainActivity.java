892d1cb954911f8502d8d24eac3bd0cd414a53f9
CMPUT301F16T19 <CMPUT301F16T19@gmail.com>
2016-11-11 15:28:17 -0700

    login functionality

--- app/src/main/java/com/c301t19/cs/ualberta/seekaride/activities/MainActivity.java
+++ app/src/main/java/com/c301t19/cs/ualberta/seekaride/activities/MainActivity.java
@@ -5,16 +5,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.c301t19.cs.ualberta.seekaride.R;
 
 
 public class MainActivity extends Activity {
 
+    EditText username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.username_Text);
+    }
+
+    public void onNewAccountClick() {
+        Intent intent = new Intent(this, NewAccountActivity.class);
+        startActivity(intent);
+    }
+
+    public void onLoginClick() {
+        String name = username.getText().toString();
+        if (name.length() <  5)
+        {
+            return;
+        }
+        Intent intent = new Intent(this, RiderActivity.class);
+        intent.putExtra("username", name);
+        startActivity(intent);
     }
 
     //remove before final build
