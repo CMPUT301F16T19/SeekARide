e710270b5dfb3a04091a52a17d0eaebcb8acbded
heiga <phillip2@ualberta.ca>
2016-11-13 17:10:06 -0700

    almost got recommended fare

--- app/src/main/java/com/c301t19/cs/ualberta/seekaride/activities/AddRequestActivity.java
+++ app/src/main/java/com/c301t19/cs/ualberta/seekaride/activities/AddRequestActivity.java
@@ -15,6 +15,18 @@ import com.c301t19.cs.ualberta.seekaride.core.Location;
 import com.c301t19.cs.ualberta.seekaride.core.Rider;
 import com.google.gson.Gson;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polyline;
+
+import java.util.ArrayList;
+
 public class AddRequestActivity extends Activity {
 
     public Button createR;
@@ -26,6 +38,8 @@ public class AddRequestActivity extends Activity {
     private TextView recommendedFare;
     private Location startLoc;
     private Location endLoc;
+    private GeoPoint startPoint;
+    private MapView map;
 
     String descriptText;
 
@@ -49,6 +63,16 @@ public class AddRequestActivity extends Activity {
                 Intent intent = new Intent(getApplicationContext(),
                         ChooseLocationActivity.class);
                 intent.putExtra("key", "start");
+                intent.putExtra("callingActivity", "add");
+                startActivityForResult(intent, RESULT_OK);
+            }
+        });
+
+        eLocation.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        ChooseLocationActivity.class);
+                intent.putExtra("key", "end");
                 intent.putExtra("callingActivity", "add");
                 startActivityForResult(intent, RESULT_OK);
             }
@@ -87,12 +111,68 @@ public class AddRequestActivity extends Activity {
     public void getLocations() {
         Intent intent = getIntent();
         Gson gson = new Gson();
+
+        // clean the map in case user changes location
+        map.getOverlays().clear();
+
         if(intent.getStringExtra("start") != null) {
             startLoc = new Location("");
             startLoc = gson.fromJson(intent.getStringExtra("start"), startLoc.getClass());
-            Toast.makeText(getApplicationContext(), startLoc.getAddress(),
-                    Toast.LENGTH_LONG).show();
+            //Toast.makeText(getApplicationContext(), startLoc.getAddress(),
+            //        Toast.LENGTH_LONG).show();
+
         }
+        if (startLoc == null) {
+            sLocation.setText("");
+        }
+        if (startLoc != null) {
+            sLocation.setText(startLoc.getAddress());
+            Marker sMarker = new Marker(map);
+            sMarker.setPosition(startLoc.getGeoLocation());
+            sMarker.setTitle(startLoc.getAddress());
+            sMarker.setIcon(getResources().getDrawable(R.drawable.person));
+            sMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+            map.getOverlays().add(sMarker);
+        }
+
+        if (intent.getStringExtra("end") != null) {
+            endLoc = new Location("");
+            endLoc = gson.fromJson(intent.getStringExtra("end"), endLoc.getClass());
+        }
+        if (endLoc == null)  {
+            eLocation.setText("");
+        }
+        if (endLoc != null) {
+            eLocation.setText(endLoc.getAddress());
+            Marker eMarker = new Marker(map);
+            eMarker.setPosition(endLoc.getGeoLocation());
+            eMarker.setTitle(endLoc.getAddress());
+            eMarker.setIcon(getResources().getDrawable(R.drawable.marker_default));
+            eMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+            map.getOverlays().add(eMarker);
+        }
+
+        if((startLoc != null) & (endLoc != null)) {
+            RoadManager roadManager = new OSRMRoadManager(this);
+            ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
+            waypoints.add(startLoc.getGeoLocation());
+            waypoints.add(endLoc.getGeoLocation());
+            Road road = roadManager.getRoad(waypoints);
+            Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+            recommendedFare.setText(calculateFare(startLoc, endLoc));
+            map.getOverlays().add(roadOverlay);
+        }
+
+        map.invalidate();
+    }
+
+    public String calculateFare(Location loc1, Location loc2) {
+        GeoPoint geo1 = loc1.getGeoLocation();
+        GeoPoint geo2 = loc2.getGeoLocation();
+        float distanceInMeters = geo1.distanceTo(geo2);
+        float distanceInKm = distanceInMeters/1000;
+        double costPerKm = 1.48;
+        return String.valueOf(distanceInKm * costPerKm);
     }
 
     @Override
@@ -101,6 +181,17 @@ public class AddRequestActivity extends Activity {
         setContentView(R.layout.activity_add_request);
         write();
         move();
+
+        // initialize display map
+        map = (MapView) findViewById(R.id.addRequestMap);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        startPoint = new GeoPoint(53.52676, -113.52715);
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        mapController.setCenter(startPoint);
+
         getLocations();
     }
 
