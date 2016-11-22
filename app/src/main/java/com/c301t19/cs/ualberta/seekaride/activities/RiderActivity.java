44e7f145202af1d1c122c3603c8f95065c440d7b
CMPUT301F16T19 <CMPUT301F16T19@gmail.com>
2016-11-22 15:21:20 -0700

    stuff:

--- app/src/main/java/com/c301t19/cs/ualberta/seekaride/activities/RiderActivity.java
+++ app/src/main/java/com/c301t19/cs/ualberta/seekaride/activities/RiderActivity.java
@@ -1,6 +1,9 @@
 package com.c301t19.cs.ualberta.seekaride.activities;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +17,8 @@ import android.widget.ListView;
 import com.c301t19.cs.ualberta.seekaride.R;
 import com.c301t19.cs.ualberta.seekaride.core.*;
 
+import java.util.Calendar;
+import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 public class RiderActivity extends Activity {
